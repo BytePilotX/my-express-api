9b15c5941ebda242189d5a3062c7342ca0b0b155
Nitro-Live <jonathanbanks792@gmail.com>
2025-08-15 16:05:29 +0100

    initial commmit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 8080;
+
+app.use( express.json() )
+
+app.listen(
+    PORT,
+    () => console.log(`Its alive on http://localhost:${PORT}`)
+);
+
+app.get('/tshirt', (req, res) => {
+    res.status(200).send({
+        brand: "Adidas",
+        age: 2
+    })
+})
+
+app.post('/tshirt/:id', (req,res) => {
+    const { id } = req.params;
+    const { brand } = req.body;
+    if (!brand) {
+        res.status(418).send({
+            message: "Body is missing"
+        })
+    }
+
+    res.send(
+        {
+            tshirt: `T-shirt with brand, ${brand} and id of, ${id}`
+        }
+    )
+})
